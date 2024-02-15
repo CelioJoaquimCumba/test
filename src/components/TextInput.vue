0626c8ffa4dfdad35a99b0cb5994e5e9fe859fc3
anchaapedro <anchaapedro@gmail.com>
2024-02-15 14:54:51 +0200

    Fix: Add method handleInput

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,23 +1,36 @@
 <template>
-  <input v-model="inputValue" :placeholder="placeholder" class="input" />
+  <input
+    v-model="inputValue"
+    :placeholder="placeholder"
+    class="input"
+    @input="handleInput"
+  />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Vue from "vue";
 
-export default Vue.extend({
+export default defineComponent({
   name: "TextInput",
   props: {
     placeholder: {
       type: String,
       default: "Digite um texto",
     },
+    value: String,
   },
   data() {
     return {
-      inputValue: "",
+      inputValue: this.value || "",
     };
+  },
+  methods: {
+    handleInput(event: Event) {
+      if (event.target instanceof HTMLInputElement) {
+        this.inputValue = event.target.value;
+        this.$emit("input", this.inputValue);
+      }
+    },
   },
 });
 </script>
