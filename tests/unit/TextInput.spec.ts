7cdc835571c30e2c73b9850b6fd8e554ae2a89dd
anchaapedro <anchaapedro@gmail.com>
2024-02-15 14:55:45 +0200

    Test( TextInput): Test for text insertion

--- tests/unit/TextInput.spec.ts
+++ tests/unit/TextInput.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import TextInput from "@/components/TextInput.vue";
+import TextInpu from "@/components/TextInput.vue";
 
 describe("TextInput.vue", () => {
   it("renders placeholder when passed", () => {
@@ -13,7 +14,6 @@ describe("TextInput.vue", () => {
     expect(wrapper.find("input").attributes("placeholder")).toBe(placeholder);
   });
 
-  //Test input insertion
   it("updates inputValue when input changes", async () => {
     const wrapper = shallowMount(TextInput);
     const input = wrapper.find("input");
@@ -21,5 +21,15 @@ describe("TextInput.vue", () => {
     await input.setValue("Hey");
 
     expect(wrapper.find("input").vm.$data.inputValue).toBe("Hey");
+  });
+
+  it("Emits input event with correct value when user types", async () => {
+    const wrapper = shallowMount(TextInput);
+    const input = wrapper.find("input");
+
+    await input.setValue("Hey");
+
+    expect(wrapper.emitted("input")).toBeTruthy();
+    expect(wrapper.emitted("input")![0][0]).toBe("Hey");
   });
 });
