16e7ffe1893baeb4b5338b27609717ec8ac277fd
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2024-02-18 15:35:23 +0200

    feat: add all molecule components

---
+++ src/components/molecules/ScoreCardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="flex bg-gray-100 rounded-lg sm:w-1/2 w-full aspect-square items-center justify-center"
+  >
+    <span class="text-2xl leading-8 font-extrabold text-green-600">{{
+      score
+    }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "scoreCardComponent",
+  props: {
+    score: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@
       <ButtonComponent value="Click me" onclick="console.log('clicked')" />
       <ProgressBar :score="10" :total="100" />
       <OptionVue :index="1" :option="1" />
+      <ScoreCardComponentVue :score="10" />
     </div>
   </div>
 </template>
@@ -15,6 +16,7 @@ import Vue from "vue";
 import ButtonComponent from "@/components/atoms/ButtonComponent.vue";
 import ProgressBar from "@/components/atoms/ProgressBar.vue";
 import OptionVue from "@/components/atoms/Option.vue";
+import ScoreCardComponentVue from "@/components/molecules/ScoreCardComponent.vue";
 
 export default Vue.extend({
   name: "HomeView",
@@ -22,6 +24,7 @@ export default Vue.extend({
     ButtonComponent,
     ProgressBar,
     OptionVue,
+    ScoreCardComponentVue,
   },
 });
 </script>
