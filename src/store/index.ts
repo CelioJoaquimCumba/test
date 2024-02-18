89f8b53467309c4e13d29bd497314b6875113dc4
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2024-02-18 15:44:41 +0200

    feat: add store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,22 @@
+import type { User } from "@/models/UserModel";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+interface State {
+  user: User | null;
+}
+
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state(): State {
+    return {
+      user: null,
+    };
+  },
+  mutations: {
+    setUser(state: State, user: User | null) {
+      state.user = user;
+    },
+  },
 });
