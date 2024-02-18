7040ac35697b64c6d5bd05477e6e9ba36970428a
CelioJoaquimCumba <37707479+CelioJoaquimCumba@users.noreply.github.com>
2024-02-18 15:48:47 +0200

    feat: add homepage of quizzy

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -12,12 +12,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import Vue from "vue";
 
-export default defineComponent({
+export default Vue.extend({
   name: "buttonComponent",
   methods: {
-    handleClick(this: any) {
+    handleClick(this: Vue) {
       this.$emit("click");
     },
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,28 +15,14 @@ export default {
     <div
       class="flex flex-col w-full sm:w-auto sm:aspect-[3/2] h-full sm:h-1/2 px-24 justify-center items-center bg-white rounded-2xl"
     >
-      <ButtonComponent value="Click me" onclick="console.log('clicked')" />
-      <ProgressBar :score="10" :total="100" />
-      <OptionVue :index="1" :option="1" />
-      <ScoreCardComponentVue :score="10" />
+      <div
+        class="flex flex-col w-full justify-center items-center max-w-xs gap-8"
+      >
+        <span class="text-3xl leading-10 font-normal text-black">QUIZ APP</span>
+        <buttonComponent @click="$router.push('/quiz')"
+          >Start Quiz</buttonComponent
+        >
+      </div>
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-import ButtonComponent from "@/components/atoms/ButtonComponent.vue";
-import ProgressBar from "@/components/atoms/ProgressBar.vue";
-import OptionVue from "@/components/atoms/Option.vue";
-import ScoreCardComponentVue from "@/components/molecules/ScoreCardComponent.vue";
-
-export default Vue.extend({
-  name: "HomeView",
-  components: {
-    ButtonComponent,
-    ProgressBar,
-    OptionVue,
-    ScoreCardComponentVue,
-  },
-});
-</script>
